bffbe5f8cbbaceb80196986f82f7603d0c150245
vivek2003cse <vivek39101@gmail.com>
2025-05-17 01:30:09 +0530

    Array

---
+++ JAVA_DSA/Array.java
@@ -0,0 +1,41 @@
+// import java.util.*;
+// public class Array {
+//     public static void update(int marks[]) {
+//         for(int i=0; i<marks.length; i++) {
+//             marks[i] = marks[i] + 1;
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int marks[] = {97, 98, 99};
+//         update(marks);
+
+//         for(int i=0; i<marks.length; i++) {
+//             System.out.print(marks[i]+" ");
+//         }
+//         System.out.println();
+//     }
+// }
+
+// Linear Search
+import java.util.*;
+public class Array {
+    public static int linearSearch(int numbers[], int key) {
+        for(int i=0; i<numbers.length; i++) {
+            if(numbers[i] == key) {
+                return i;
+            }
+        }
+            return -1;
+        }
+          public static void main(String args[]) {
+            int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
+            int key = 14;
+
+            int index = linearSearch(numbers, key);
+            if(index == -1) {
+                System.out.println("NOT found");
+            } else {
+                System.out.println("key is at index : "+ index);
+            }
+    }
+}
