0feb56514eb81691a870f021c404f1f036470c97
vivek2003cse <vivek39101@gmail.com>
2025-05-17 01:35:38 +0530

    Patterns

--- Java_BASICS/Functions.java
+++ Java_BASICS/Functions.java
@@ -202,22 +202,22 @@
 
 // Decimal to Binary
 
-public class Functions{ 
-    public static void decToBin(int n) {
-        int num = n;
-        int pow = 0;
-        int binNum = 0;
-        while(n > 0) {
-            int rem = n % 2;
-            binNum = binNum + (rem * (int)Math.pow(10, pow));
-            pow++;
-            n = n/2;
-        }
-        System.out.println("binary form of " + num + " = "+ binNum);
-    }
-    public static void main(String args[]) {
-        int n = 13;
-        decToBin(n);
-    }
-}
+// public class Functions{ 
+//     public static void decToBin(int n) {
+//         int num = n;
+//         int pow = 0;
+//         int binNum = 0;
+//         while(n > 0) {
+//             int rem = n % 2;
+//             binNum = binNum + (rem * (int)Math.pow(10, pow));
+//             pow++;
+//             n = n/2;
+//         }
+//         System.out.println("binary form of " + num + " = "+ binNum);
+//     }
+//     public static void main(String args[]) {
+//         int n = 13;
+//         decToBin(n);
+//     }
+// }
 
--- Java_BASICS/Patterns.java
+++ Java_BASICS/Patterns.java
@@ -139,26 +139,53 @@
 // }
 
 // Ḥollow Rhombus
+// public class Patterns {
+//     public static void hollow_rohmbus(int n) {
+//         for(int i=1; i<=n; i++) {
+//             for(int j=1; j<=n-i; j++) {
+//                 System.out.print(" ");
+//             }
+//             for(int j=1; j<=n; j++) {
+//                 if(i==1 || i==n || j == 1 || j == n) {
+//                 System.out.print("*");
+//                     } else {
+//                         System.out.print(" ");
+//                     }
+//                 }
+//             System.out.println();
+//         }
+//     }
+//     public static void main(String[] args) {
+//         hollow_rohmbus(4);
+//     }
+// } 
+
+// Diamond Problem
 public class Patterns {
-    public static void hollow_rohmbus(int n) {
+    public static void diamond(int n) {
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=n-i; j++) {
                 System.out.print(" ");
             }
-            for(int j=1; j<=n; j++) {
-                if(i==1 || i==n || j == 1 || j == n) {
+            for(int j=1; j<=2*i-1; j++) {
                 System.out.print("*");
-                    } else {
-                        System.out.print(" ");
-                    }
-                }
+            }
+            System.out.println();
+        }
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=2*i-1; j++) {
+                System.out.print("*");
+            }
             System.out.println();
         }
     }
-    public static void main(String[] args) {
-        hollow_rohmbus(5);
+    public static void main(String args[]) {
+        diamond(4);
     }
-} 
+}
 
 
 
