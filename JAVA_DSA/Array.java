2a1597c73f3740de794d35951991401c1143a1f2
vivek2003cse <vivek39101@gmail.com>
2025-05-17 17:42:23 +0530

    Array

--- JAVA_DSA/Array.java
+++ JAVA_DSA/Array.java
@@ -17,25 +17,43 @@
 // }
 
 // Linear Search
+// import java.util.*;
+// public class Array {
+//     public static int linearSearch(int numbers[], int key) {
+//         for(int i=0; i<numbers.length; i++) {
+//             if(numbers[i] == key) { 
+//                 return i;
+//             }
+//         }
+//             return -1;
+//         }
+//         public static void main(String args[]) {
+//             int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
+//             int key = 14;
+
+//             int index = linearSearch(numbers, key);
+//             if(index == -1) {
+//                 System.out.println("NOT found");
+//             } else {
+//                 System.out.println("key is at index : "+ index);
+//         }
+//     }
+// }
+
 import java.util.*;
 public class Array {
-    public static int linearSearch(int numbers[], int key) {
+    public static int getLargest(int numbers[]) {
+        int largest = Integer.MIN_VALUE;
         for(int i=0; i<numbers.length; i++) {
-            if(numbers[i] == key) {
-                return i;
+            if(largest < numbers[i]) {
+                largest = numbers[i];
             }
         }
-            return -1;
-        }
-          public static void main(String args[]) {
-            int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
-            int key = 14;
+        return largest;
+    }
+    public static void main(String args[]) {
+        int numbers[] = {1, 2, 6, 3, 5};
+        System.out.println("Largest value is : " + getLargest(numbers));
 
-            int index = linearSearch(numbers, key);
-            if(index == -1) {
-                System.out.println("NOT found");
-            } else {
-                System.out.println("key is at index : "+ index);
-            }
     }
 }
